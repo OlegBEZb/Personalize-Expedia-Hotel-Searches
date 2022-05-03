450c191d06f040a865b2ff84026839b678f2e1b5
Oleg Litvinov <olegolego@mail.ru>
2022-05-03 14:00:47 +0200

    added grouped train test split;

---
+++ utils.py
@@ -0,0 +1,17 @@
+from sklearn.model_selection import train_test_split
+
+
+def train_test_group_split(*arrays,
+                           group_array=None,
+                           train_size=None):
+    """
+
+
+    Example
+    X_train2, X_test2, y_train2, y_test2, groups_train2, groups_test2 = train_test_group_split(X, y, groups, group_array=groups, train_size=0.9)
+    """
+
+    grp_changes = group_array.shift() != group_array
+    grp_changes_train_approx = grp_changes.iloc[:int(len(grp_changes) * train_size)]
+    split_index = grp_changes_train_approx[grp_changes_train_approx].last_valid_index()
+    return train_test_split(*arrays, train_size=split_index, shuffle=False)
