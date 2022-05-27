18107e17a106ffaddbc8b8948d0982e83e611fb4
Oleg Litvinov <olegolego@mail.ru>
2022-05-27 17:13:48 +0200

    extended to 150 features as started converge

--- dstack_catboost_ranker.py
+++ dstack_catboost_ranker.py
@@ -35,7 +35,7 @@ cols_to_use = [c for c in cols_to_use if c not in cols_lost_from_v1]
 #### remove me
 from random import shuffle
 shuffle(cols_to_use)
-cols_to_use = cols_to_use[:100]
+cols_to_use = cols_to_use[:150]
 cols_to_use = list(set(cols_to_use + ['srch_id', 'prop_id']))
 #### remove above
 
